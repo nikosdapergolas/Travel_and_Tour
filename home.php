60615544ce385cd8a468eacff4e040efc5afc8be
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2022-10-28 13:47:11 +0300

    changed dir again(4th time)

--- home.php
+++ home.php
@@ -27,7 +27,7 @@
             <nav class="navbar">
                 <a href="home.php">Home</a>
                 <a href="about.php">About</a>
-                <a href="package.php">Package/a>
+                <a href="package.php">Package</a>
                 <a href="book.php">Book</a>
             </nav>
 
