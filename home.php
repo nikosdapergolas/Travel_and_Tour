592d5f394152bfd606d320242fa91ffd58b45457
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2022-10-29 12:57:47 +0300

    contact info and stuff

--- home.php
+++ home.php
@@ -51,6 +51,48 @@
 
 
 
+        <!-- footer section starts -->
+
+        <section class="footer">
+
+            <div class="box-container">
+
+                <div class="box">
+                    <h3>Quick links</h3>
+                    <a href="home.php"><i class="fas fa-angle-right"></i> Home</a>
+                    <a href="about.php"><i class="fas fa-angle-right"></i> About</a>
+                    <a href="package.php"><i class="fas fa-angle-right"></i> Package</a>
+                    <a href="book.php"><i class="fas fa-angle-right"></i> Book</a>
+                </div>
+
+                <div class="box">
+                    <h3>Extra links</h3>
+                    <a href="#"><i class="fas fa-angle-right"></i> Ask questions</a>
+                    <a href="#"><i class="fas fa-angle-right"></i> About us</a>
+                    <a href="#"><i class="fas fa-angle-right"></i> Privacy policy</a>
+                    <a href="#"><i class="fas fa-angle-right"></i> Terms of use</a>
+                </div>
+
+                <div class="box">
+                    <h3>Contact info</h3>
+                    <a href="#"><i class="fas fa-phone"></i> +123-456-7890 </a>
+                    <a href="#"><i class="fas fa-phone"></i> +123-456-7890 </a>
+                    <a href="#"><i class="fas fa-envelope"></i> travel@gmail.com </a>
+                    <a href="#"><i class="fas fa-map"></i> Athens, Greece - 12345 </a>
+                </div>
+
+                <div class="box">
+                    <h3>Follow us</h3>
+                    <a href="#"><i class="fab fa-facebook-f"></i> Tacebook </a>
+                    <a href="#"><i class="fab fa-twitter"></i> Twitter </a>
+                    <!-- 21.13 -->
+                </div>
+
+            </div>
+
+        </section>
+
+        <!-- footer section ends -->
 
         <!-- Swiper js link -->
         <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
