d1c467d12ed4ac19c66fd53d0d87e01afd92bc46
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2022-10-30 13:05:25 +0200

    Saved at a state before beggining home section

--- php/home.php
+++ php/home.php
@@ -37,49 +37,51 @@
 
         <!-- header sections ends -->
 
+        <!-- 32.58 -->
+
         <!-- services section starts  -->
 
-<section class="services">
+        <section class="services">
 
-    <h1 class="heading-title"> our services </h1>
+            <h1 class="heading-title"> our services </h1>
 
-    <div class="box-container">
+            <div class="box-container">
 
-        <div class="box">
-            <img src="../images/icon-1.png" alt="">
-            <h3>adventure</h3>
-        </div>
+                <div class="box">
+                    <img src="../images/icon-1.png" alt="">
+                    <h3>adventure</h3>
+                </div>
 
-        <div class="box">
-            <img src="images/icon-2.png" alt="">
-            <h3>tour guide</h3>
-        </div>
+                <div class="box">
+                    <img src="../images/icon-2.png" alt="">
+                    <h3>tour guide</h3>
+                </div>
 
-        <div class="box">
-            <img src="images/icon-3.png" alt="">
-            <h3>trekking</h3>
-        </div>
+                <div class="box">
+                    <img src="../images/icon-3.png" alt="">
+                    <h3>trekking</h3>
+                </div>
 
-        <div class="box">
-            <img src="images/icon-4.png" alt="">
-            <h3>camp fire</h3>
-        </div>
+                <div class="box">
+                    <img src="../images/icon-4.png" alt="">
+                    <h3>camp fire</h3>
+                </div>
 
-        <div class="box">
-            <img src="images/icon-5.png" alt="">
-            <h3>off road</h3>
-        </div>
+                <div class="box">
+                    <img src="../images/icon-5.png" alt="">
+                    <h3>off road</h3>
+                </div>
 
-        <div class="box">
-            <img src="images/icon-6.png" alt="">
-            <h3>camping</h3>
-        </div>
+                <div class="box">
+                    <img src="../images/icon-6.png" alt="">
+                    <h3>camping</h3>
+                </div>
 
-    </div>
+            </div>
 
-</section>
+        </section>
 
-<!-- services section ends -->
+        <!-- services section ends -->
 
 
 
