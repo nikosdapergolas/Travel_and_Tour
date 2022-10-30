63ad86e16aa508a96c04d73b0db03887560648b3
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2022-10-30 12:43:09 +0200

    changed other files too

---
+++ php/book.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Book</title>
+
+        <!-- Swiper css link -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+
+        <!--Font awsome cdn link-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+        <!--Custom css file link-->
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+
+    <body>
+        
+        <!-- header sections starts -->
+
+        <section class="header">
+            <a href="home.php" class="logo">travel.</a>
+
+            <nav class="navbar">
+                <a href="home.php">Home</a>
+                <a href="about.php">About</a>
+                <a href="package.php">Package</a>
+                <a href="book.php">Book</a>
+            </nav>
+
+            <div id="menu-btn" class="fas fa-bars"></div>
+
+        </section>
+
+        <!-- header sections ends -->
+
+        <div class="heading" style="background:url(../images/header-bg-3.png) no-repeat">
+            <h1>Book now</h1>
+        </div>
+
+        <!-- about section starts  -->
+
+
+
+        
+
+
+
+
+
+
+
+
+
+        <!-- footer section starts -->
+
+        <section class="footer">
+
+            <div class="box-container">
+
+                <div class="box">
+                    <h3>Quick links</h3>
+                    <a href="home.php"><i class="fas fa-angle-right"></i> Home</a>
+                    <a href="about.php"><i class="fas fa-angle-right"></i> About</a>
+                    <a href="package.php"><i class="fas fa-angle-right"></i> Package</a>
+                    <a href="book.php"><i class="fas fa-angle-right"></i> Book</a>
+                </div>
+
+                <div class="box">
+                    <h3>Extra links</h3>
+                    <a href="#"><i class="fas fa-angle-right"></i> Ask questions</a>
+                    <a href="#"><i class="fas fa-angle-right"></i> About us</a>
+                    <a href="#"><i class="fas fa-angle-right"></i> Privacy policy</a>
+                    <a href="#"><i class="fas fa-angle-right"></i> Terms of use</a>
+                </div>
+
+                <div class="box">
+                    <h3>Contact info</h3>
+                    <a href="#"><i class="fas fa-phone"></i> +123-456-7890 </a>
+                    <a href="#"><i class="fas fa-phone"></i> +123-456-7890 </a>
+                    <a href="#"><i class="fas fa-envelope"></i> travel@gmail.com </a>
+                    <a href="#"><i class="fas fa-map"></i> Athens, Greece - 12345 </a>
+                </div>
+
+                <div class="box">
+                    <h3>Follow us</h3>
+                    <a href="#"><i class="fab fa-facebook-f"></i> Tacebook </a>
+                    <a href="#"><i class="fab fa-twitter"></i> Twitter </a>
+                    <a href="#"><i class="fab fa-instagram"></i> Instagram </a>
+                    <a href="#"><i class="fab fa-linkedin"></i> Linkedin </a>
+                </div>
+
+            </div>
+
+            <div class="credit"> created by <span>Nikos Dapergolas</span> | all rights reserved! </div>
+
+        </section>
+
+        <!-- footer section ends -->
+
+        <!-- Swiper js link -->
+        <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+        <!--Custom js file link-->
+        <script src="../js/script.js"></script>
+
+    </body>
+</html>
--- php/home.php
+++ php/home.php
@@ -37,6 +37,50 @@
 
         <!-- header sections ends -->
 
+        <!-- services section starts  -->
+
+<section class="services">
+
+    <h1 class="heading-title"> our services </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="../images/icon-1.png" alt="">
+            <h3>adventure</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-2.png" alt="">
+            <h3>tour guide</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-3.png" alt="">
+            <h3>trekking</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-4.png" alt="">
+            <h3>camp fire</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-5.png" alt="">
+            <h3>off road</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/icon-6.png" alt="">
+            <h3>camping</h3>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- services section ends -->
+
 
 
 
@@ -90,6 +134,8 @@
                 </div>
 
             </div>
+
+            <div class="credit"> created by <span>Nikos Dapergolas</span> | all rights reserved! </div>
 
         </section>
 
