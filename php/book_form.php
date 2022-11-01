b4e34c852fbde4ce181e5169433b8356da7d9fb4
Nikos Dapergolas <nikos.2001@outlook.com.gr>
2022-11-01 19:14:24 +0200

    Finished everything

--- php/book_form.php
+++ php/book_form.php
@@ -3,22 +3,25 @@
    $connection = mysqli_connect('localhost','root','','book_db');
 
    if(isset($_POST['send'])){
-      $name = $_POST['name'];
-      $email = $_POST['email'];
-      $phone = $_POST['phone'];
-      $address = $_POST['address'];
-      $location = $_POST['location'];
-      $guests = $_POST['guests'];
-      $arrivals = $_POST['arrivals'];
-      $leaving = $_POST['leaving'];
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $location = $_POST['location'];
+        $guests = $_POST['guests'];
+        $arrivals = $_POST['arrivals'];
+        $leaving = $_POST['leaving'];
 
-      $request = " insert into book_form(name, email, phone, address, location, guests, arrivals, leaving) values('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving') ";
-      mysqli_query($connection, $request);
+        $request = " insert into book_form(name, email, phone, address, location, guests, arrivals, leaving) values('$name','$email','$phone','$address','$location','$guests','$arrivals','$leaving') ";
+        mysqli_query($connection, $request);
 
-      header('location:book.php'); 
-
-   }else{
-      echo 'something went wrong please try again!';
-   }
+        header('location:book.php');
+        
+        echo '<script>alert("Your booking was successfull. See you there!!")</script>';
+    }
+    else
+    {
+        echo 'something went wrong please try again!';
+    }
 
 ?>
